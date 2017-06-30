d27fd67e00f1d110a95c8750850d44d3acd90118
Josh Martin <joshmartin5001@gmail.com>
2017-06-30 13:57:37 -0400

    refactored code

--- app.js
+++ app.js
@@ -6,23 +6,12 @@ const app = {
     this.flicks = [] // stores the flicks and maybe a few chicks
     this.max = 0
     this.list = document.querySelector(selectors.listSelector)
+    this.template = document.querySelector(selectors.templateSelector)
 
     document
       .querySelector(selectors.formSelector) // takes in the form past in and selects it
       .addEventListener('submit', this.handleSubmit.bind(this)) // adds a listion for when the button with the name `submit` is clicked, then makes a called to `handleSubmit`
   }, // end of init
-  refresher(){
-    while(this.list.firstChild){ // removes the old state to the dom
-      this.list.removeChild(this.list.firstChild)
-    }
-    // this.list.classList.remove('animated','fadeOutDown')
-    // this.list.classList.add('animated','fadeInUp')
-
-    this.flicks.map((lists, i)=>{ // push the new state to the dom
-      // console.log(lists)
-      this.list.appendChild( this.renderListItem(lists))
-    })
-  },
    sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
@@ -31,136 +20,90 @@ const app = {
     }
   }
 },
-  favorited(ev){
-    const searchMovie =ev.target.dataset.flick
-    // console.log(this.flicks)
-    this.flicks.map((item, i)=>{
-       if(item.name == searchMovie){
-         item.isFavorited = !item.isFavorited
-         ev.target.classList.toggle('favorited')
-         if(item.isFavorited){
+  favorited(flick,ev){
+    const listItem = ev.target
+    // debugger
+         flick.isFavorited =listItem.classList.toggle('favorited')
+
+         if(flick.isFavorited){
            ev.target.textContent = "😍"
          }
          else{
            ev.target.textContent = "Favorite"
          }
-       }
-    } )
 
-    console.log(this.flicks)
+  },// end of favorited
+  remover(flick,ev){
+    // remove from screen
+    console.log(ev)
+    const listItem =ev.target.closest('.list-item')
+    listItem.remove()
 
-  },
-  remover(ev){
-    const searchMovie =ev.target.dataset.flick
-    this.flicks.map((item, i)=>{
-       if(item.name == searchMovie){
-         this.flicks.splice(i,1)
-    } }) // end of map
 
-    // this.list.classList.add('animated','fadeOutDown')
-    this.refresher()
+    // remove from array
+    const i = this. flicks.indexOf(flick)
+    this.flicks.splice(i,1)
   }, // end of remover
   moveUp(ev){
+    const listItem =ev.target.closest('.list-item')
+    const parentItem = listItem.parentNode
     // debugger
-    Array.prototype.insert = function ( index, item ) {
-    this.splice( index, 0, item );
-};
-    const searchMovie =ev.target.dataset.flick
-    this.flicks.map((item, i)=>{
-       if(item.name == searchMovie && i!= 0 ){
-         const pushItem = item
-         this.flicks.splice(i,1)
-         this.flicks.insert(i-1,pushItem)
-    } }) // end of map
-    this.refresher()
+    if(parentItem.firstElementChild == listItem){
+      console.log("You can't move it like that!!!");
+    }
+    else{
+    const preItem = listItem.previousSibling
+    const moveItem= listItem;
+
+    parentItem.insertBefore(preItem,listItem)
+    listItem.remove()
+    parentItem.insertBefore(moveItem,preItem)
+  }
+
   },
   moveDown(ev){
     // debugger
-    Array.prototype.insert = function ( index, item ) {
-    this.splice( index, 0, item );
-};
-    const searchMovie =ev.target.dataset.flick
-    this.flicks.map((item, i)=>{
-       if(item.name == searchMovie && i!= this.flicks.length ){
-         const pushItem = item
-         this.flicks.splice(i,1)
-         this.flicks.insert(i+1,pushItem)
-    } }) // end of map
-    this.refresher()
+    const listItem =ev.target.closest('.list-item')
+    const parentItem = listItem.parentNode
+    
+    if(parentItem.lastElementChild == listItem){
+      console.log("You can't move it like that!!!");
+    }
+    else{
+    const preItem = listItem.nextSibling
+    const moveItem= listItem;
+    parentItem.insertBefore(moveItem,preItem)
+    preItem.remove()
+    parentItem.insertBefore(preItem,listItem)
+    }
   },
   /*
   * @desc - creates an entry of a flick
   * @returns - an instence of an li flick
   */
   renderListItem(flick) {
-/*
-    <li class="cell list-item">
-          <div class="grid-x">
-        <div class="medium-6 large-4 cell">
-        // text
-        </div>
-        <div class="button-group">
-        <button class="warning button">Favorite</button>
-        <button class="alert button">remove</button>
-        <button class="secondary button">move up</button>
-        <button class="secondary button">move down</button>
-        </div>
-      </div>
-    </li>
-*/
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = flick.id
+    item
+      .querySelector('.text-div')
+      .textContent = _.startCase(flick.name)
 
-    const item = document.createElement('li')
-    item.classList.add('cell','list-item','animated')
+    item
+      .querySelector('button.favorite')
+      .addEventListener('click',this.favorited.bind(this,flick))
 
-      const divItem = document.createElement('div')
-      divItem.classList.add('grid-x')
+    item
+      .querySelector('button.remove')
+      .addEventListener('click',this.remover.bind(this,flick))
 
-        const textDiv = document.createElement('div')
-        textDiv.classList.add('auto', 'cell','text-div')
-          textDiv.textContent = _.startCase(flick.name)
-          divItem.appendChild(textDiv)
+    item
+      .querySelector('button.move-up')
+      .addEventListener('click', this.moveUp.bind(this))
 
-        const buttonGroupDiv = document.createElement('div')
-        buttonGroupDiv.classList.add('button-group')
-
-          const favButton = document.createElement('button')
-          favButton.classList.add('warning','button')
-            favButton.textContent = "Favorite"
-            favButton.dataset.flick = flick.name;
-
-            favButton.addEventListener("click",this.favorited.bind(this))
-            buttonGroupDiv.appendChild(favButton)
-
-          const removeButton = document.createElement('button')
-          removeButton.classList.add('alert','button')
-            removeButton.textContent = "Remove"
-
-            removeButton.dataset.flick = flick.name;
-            removeButton.addEventListener('click',this.remover.bind(this))
-            buttonGroupDiv.appendChild(removeButton)
-
-          const upButton = document.createElement('button')
-          upButton.classList.add('secondary','button')
-            upButton.textContent = "Move Up"
-            upButton.dataset.flick = flick.name;
-            upButton.addEventListener('click',this.moveUp.bind(this))
-            buttonGroupDiv.appendChild(upButton)
-
-          const downButton = document.createElement('button')
-          downButton.classList.add('secondary','button')
-            downButton.textContent = "Move Down"
-            downButton.dataset.flick = flick.name;
-            downButton.addEventListener('click',this.moveDown.bind(this))
-            buttonGroupDiv.appendChild(downButton)
-
-        divItem.appendChild(textDiv)
-        divItem.appendChild(buttonGroupDiv)
-
-      item.appendChild(divItem)
-      item.classList.add('animated','fadeInUp')
-
-
-
+    item
+      .querySelector('button.move-down')
+      .addEventListener('click', this.moveDown.bind(this))
     return item
   }, // end of renderListItem
 
@@ -173,12 +116,16 @@ const app = {
       name: f.flickName.value,
       isFavorited: false,
     } // end of flick obj
-    f.flickName.value =""
-    this.flicks.push(flick) // puts the new flicks into the array
-    this.refresher()
+    // f.flickName.value =""
+    this.flicks.unshift(flick) // puts the new flicks into the beginning of the array
+
+    const listItem = this.renderListItem(flick)
+    this.list
+    .insertBefore(listItem,this.list.firstElementChild)
     // console.log(this.flicks)
 
     this.max ++
+    f.reset()
   }, // end of handleSubmit
 } // end of the `app` object
 
@@ -187,4 +134,5 @@ app.init({ // the contents of this get past to the app object.
           // NOTE: this runs as soon as the page loads
   formSelector: 'form#flick-form',
   listSelector: '#flick-list',
+  templateSelector: '.list-item.template',
 })
